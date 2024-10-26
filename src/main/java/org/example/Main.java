9dea272e9f5b50315b5fabcd18b430ef3a322f21
Mykola <omelka3318@gmail.com>
2024-10-26 08:23:01 +0300

    step

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,13 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcomdde!");
-
+// step 1 що портрібно використати в проекті:
+        //які цикли. змінні, створити роботу по шифру.
+        //step 2 створити клаас та метод котрий буде зчитувати файл та перезаписувати згідно шифру
+        // step 3 до шифру додати ключі для читання шифру
+        //step 4 додати масив хочаб з одного алфавіту
+        //step 5 кодувати спец символи
+        // step 6 створити взаємодію з користувачем на введеня тексту
+        //step 7  зіброати все докупи щоб працювало.
         }
     }

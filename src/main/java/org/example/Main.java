357c731eefad5ea944cbd5d1dc5b8dd4a9e9ec22
Mykola <omelka3318@gmail.com>
2024-10-29 21:51:23 +0200

    commit args

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,42 @@
 package org.example;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        if (args == null) {
-            System.out.println("input is incorrect");
+
+        if (args.length != 3) {
+            System.out.println("Expected 3 args, not but receive :" + args.length);
             return;
         }
-        for (String input:
-             args) {
-            System.out.println("input is correct"+ input);
+
+        String command = args[0];
+        String path = args[1];
+        String key = args[2];
+        Command[] cmdValues = Command.values();
+        Command inputCmd = null;
+        for (Command cmd : cmdValues) {
+            if (cmd.toString().equals(command)) {
+                inputCmd = cmd;
+            }
+        }
+        if (inputCmd == null) {
+            System.out.println("incorrect command:" + command);
+            return;
+        }
+        if (!Files.exists(Path.of(path))) {
+            System.out.println("not found path: "+path);
+            return;
+        }
+        int keyAsNumber;
+        try  {
+            keyAsNumber=Integer.parseInt(key);
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException on "+key);
+            return;
         }
     }
 }
